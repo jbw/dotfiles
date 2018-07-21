613351dee0e87877afb4e6145b3f4b193576ab7a
cupcake <jbw@users.noreply.github.com>
2018-07-21 14:34:43 +0100

    use theme submodule

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ ln -s $(pwd)/zshrc ~/.zshrc
 ln -s $(pwd)/oh-my-zsh ~/.oh-my-zsh
 
 # Install themes
-ln -s $(pwd)/themes/dracula/dracula.zsh-theme $(pwd)/oh-my-zsh/themes/dracula.zsh-theme
+ln -s $(pwd)/zsh/dracula.zsh-theme $(pwd)/oh-my-zsh/themes/dracula.zsh-theme
