f640e5ba94cc2c8a29928fd9ec8be631ba9fba55
cupcake <jbw@users.noreply.github.com>
2018-07-21 14:32:25 +0100

    doot doot doot

---
+++ install.sh
@@ -0,0 +1,7 @@
+
+# Create symbolic links for zsh
+ln -s $(pwd)/zshrc ~/.zshrc
+ln -s $(pwd)/oh-my-zsh ~/.oh-my-zsh
+
+# Install themes
+ln -s $(pwd)/themes/dracula/dracula.zsh-theme $(pwd)/oh-my-zsh/themes/dracula.zsh-theme
