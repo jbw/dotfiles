a7e0627590c92804620b11a99e0523f28dccfa36
cupcake <jbw@users.noreply.github.com>
2018-08-10 22:20:28 +0100

    ignore files and remove i3 dir

--- make.sh
+++ make.sh
@@ -1,17 +1,17 @@
 
-files=($(find . -maxdepth 1 -type f -not -path '*/\.*' ! -path './make.sh' -printf "%f\n"))
+files=($(find . -maxdepth 1 -type f -not -path '*/\.*' ! -path './make.sh' ! -path './README.md' ! -path './install.sh' -printf "%f\n"))
 directories=($(find . -maxdepth 1 -type d -not -path '*/\.*' ! -path . -printf "%f\n"))
 
 for f in "${files[@]}"
 do
-   echo "ln -s $(pwd)/$f ~/.$f"
+   echo "ln -sf $(pwd)/$f ~/.$f"
 done
 
 for d in "${directories[@]}"
 do
-    echo "ln -s $(pwd)/$d ~/.$d"
+    echo "ln -sf $(pwd)/$d ~/.$d"
 done
 
 #Install themes
-echo "ln -s $(pwd)/zsh/dracula.zsh-theme $(pwd)/oh-my-zsh/themes/dracula.zsh-theme"
+echo "ln -sf $(pwd)/zsh/dracula.zsh-theme $(pwd)/oh-my-zsh/themes/dracula.zsh-theme"
 
