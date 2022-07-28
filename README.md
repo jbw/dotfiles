071cf3bd6ec5753d240f751b5f5afa3f61b45622
Jason Watson <jbw@jbw.cc>
2022-07-28 18:22:49 +0100

    chore: initial start on nix setup

--- README.md
+++ README.md
@@ -1,10 +1,1 @@
 # dotfiles
-
-:computer: configs 
-
-`./install.sh`
-
-# Configs
-* [VS Code](config/vscode/README.md)
-
-:rice:
--- install.sh
+++
@@ -1,4 +0,0 @@
-rm -f install_list.sh && \
-./print_install.sh >> install_list.sh && \
-chmod +x install_list.sh && \
-./install_list.sh
