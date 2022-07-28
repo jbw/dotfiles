e2f4d769b9ded53e58d63c9e6fafc34cffdde38a
Jason Watson <jbw@jbw.cc>
2022-07-28 21:08:26 +0100

    refactor: install nix-darwin

--- install/nix-darwin.sh
+++ install/nix-darwin.sh
@@ -2,9 +2,11 @@
 
 install_nix_darwin() {
   if command -v darwin-rebuild &>/dev/null; then
-    echo "Already installed Darwin Nix."
+    echo "Already installed Nix Darwin."
   else
-    echo "Installing Dawrin Nix..."
+    echo "Installing Nix Darwin..."
     nix-build https://github.com/LnL7/nix-darwin/archive/master.tar.gz -A installer ./result/bin/darwin-installer
+
+    sudo mv /etc/nix/nix.conf /etc/nix/nix.conf.backup-before-nix-darwin
   fi
 }
