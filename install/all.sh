a22b3531c3e8c5b7e63ee5acbee8446b9f248872
Jason Watson <jbw@jbw.cc>
2022-07-28 21:09:03 +0100

    refactor: run new steps

--- install/all.sh
+++ install/all.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 {
+  set -euo pipefail
+
   source ./install/nix.sh
   source ./install/nix-darwin.sh
+  source ./install/configs.sh
+  source ./install/system.sh
 
   install_nix
   install_nix_darwin
+  install_my_configs
+  build
 }
