b8079851c05ed71e93f6a35f98f223777c60468a
Jason Watson <jbw@jbw.cc>
2022-07-31 01:16:21 +0100

    refactor: add sudo_prompt

--- install.sh
+++ install.sh
@@ -2,6 +2,18 @@
 {
   set -euo pipefail
 
+  # Lifted from https://github.com/kalbasit/shabka/blob/8f6ba74a9670cc3aad384abb53698f9d4cea9233/os-specific/darwin/setup.sh#L22
+  sudo_prompt() {
+    echo "Please enter your password for sudo authentication"
+    sudo -k
+    sudo echo "sudo authenticaion successful!"
+    while true; do
+      sudo -n true
+      sleep 60
+      kill -0 "$$" || exit
+    done 2>/dev/null &
+  }
+
   install_command_line_tools() {
     # Unattended installation of Command Line Tools
     # Found on apple.stackexchange.com at https://apple.stackexchange.com/questions/107307/how-can-i-install-the-command-line-tools-completely-from-the-command-line/195963#195963
@@ -75,6 +87,7 @@
     darwin-rebuild switch -I "darwin-config=$HOME/.nixpkgs/darwin-configuration.nix"
   }
 
+  sudo_prompt
   install_command_line_tools
   install_nix
   install_nix_darwin
